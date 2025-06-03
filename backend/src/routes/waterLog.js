e83d37dc6ba27691e5f6ed1f2a727e0f7b93b56a
kei0101xw <kei0101xw@icloud.com>
2025-06-04 02:17:55 +0900

    [add] waterLogのルーティングをした

--- backend/src/routes/waterLog.js
+++ backend/src/routes/waterLog.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { createWaterLog } = require("../controllers/waterLog");
+const verifyToken = require("../middleware/verifyToken");
+
+// :petId をパスパラメータとして受け取る
+router.post("/:petId", verifyToken, createWaterLog);
 
 module.exports = router;
