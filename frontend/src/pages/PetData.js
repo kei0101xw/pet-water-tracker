e1d674681a8e4928a50bf02f768b3cbbdbdb13fd
ToFuruta <tomohiro.9.10@icloud.com>
2025-06-04 11:39:53 +0900

    [add] PetDataの追加

---
+++ frontend/src/pages/PetData.js
@@ -0,0 +1,29 @@
+export const petList = [
+  {
+    id: 1,
+    name: "ココア",
+    dogcat: "犬",
+    type: "柴犬",
+    age: 3,
+    weight: 10000,
+    birthday: "2025-06-04",
+  },
+  {
+    id: 2,
+    name: "ミケ",
+    dogcat: "犬",
+    type: "トイプードル",
+    age: 9,
+    weight: 10000,
+    birthday: "2025-06-03",
+  },
+  {
+    id: 3,
+    name: "ピーちゃん",
+    dogcat: "犬",
+    type: "チワワ",
+    age: 6,
+    weight: 10000,
+    birthday: "2025-05-20",
+  },
+];
