ab54d748bcde9b569c3435c82549492c96e42727
ToFuruta <tomohiro.9.10@icloud.com>
2025-06-04 09:48:16 +0900

    [change] ルーティングの修正・追加

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Analysis from "./pages/Analysis";
+import Detail from "./pages/Detail";
+import Setting from "./pages/Setting";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 
@@ -10,7 +14,11 @@ function App() {
       <Header />
       <div>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/analysis" element={<Analysis />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/setting" element={<Setting />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
