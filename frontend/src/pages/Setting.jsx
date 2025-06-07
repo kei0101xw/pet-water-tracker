37c5e6b8995fabd6791694ebefa2d8b19207e19c
ToFuruta <tomohiro.9.10@icloud.com>
2025-06-08 00:13:31 +0900

    [change]ルーティングの変更

--- frontend/src/pages/Setting.jsx
+++ frontend/src/pages/Setting.jsx
@@ -9,10 +9,7 @@ const Setting = () => {
     <div className="setting-container">
       <h1 className="setting-title">設定メニュー</h1>
       <div className="setting-buttons">
-        <button
-          onClick={() => navigate("/setting/user")}
-          className="setting-button"
-        >
+        <button onClick={() => navigate("/user")} className="setting-button">
           ユーザー情報
         </button>
         <button
