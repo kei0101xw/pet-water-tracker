7324833e9bf46194b79ec247e0337ccbf8d66fce
ToFuruta <tomohiro.9.10@icloud.com>
2025-06-08 13:16:03 +0900

    [add]PetPageファイルの追加

---
+++ frontend/src/pages/PetPage.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../pages/PetPage.css";
+
+const PetPage = () => {
+  const navigate = useNavigate();
+
+  const handleRegisterPet = () => {
+    navigate("/pet/register");
+  };
+
+  const handlePetSettings = () => {
+    navigate("/pet/settings");
+  };
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">ペット管理ページ</h1>
+      <div className="buttonGroup">
+        <button onClick={handleRegisterPet} className="registerButton">
+          ペットの登録
+        </button>
+        <button onClick={handlePetSettings} className="settingsButton">
+          ペットの設定
+        </button>
+      </div>
+      <div className="backButtonWrapper">
+        <button onClick={handleGoBack} className="backButton">
+          戻る
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PetPage;
