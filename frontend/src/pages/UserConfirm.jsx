1bde5ebe300e9903ead71a18a411af6ed34a3742
ToFuruta <tomohiro.9.10@icloud.com>
2025-06-08 18:49:56 +0900

    [feat]ユーザー登録機能の追加

--- frontend/src/pages/UserConfirm.jsx
+++ frontend/src/pages/UserConfirm.jsx
@@ -12,21 +12,21 @@ const UserConfirm = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch("/api/register", {
+      const res = await fetch("http://localhost:4000/api/v1/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: "include",
         body: JSON.stringify(user),
       });
 
-      if (!response.ok) {
+      if (!res.ok) {
         throw new Error("登録に失敗しました");
       }
 
-      // 成功時の処理
       alert("登録が完了しました！");
-      navigate("/home"); // ホーム画面などにリダイレクト
+      navigate("/");
     } catch (error) {
       alert(error.message);
     }
