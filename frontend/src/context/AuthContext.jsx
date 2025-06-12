c4edfeeea4de3fead485523258cf99429b5cfc84
kei0101xw <kei0101xw@icloud.com>
2025-06-13 02:43:52 +0900

    [add] リロードしてもホームに飛ばないように設定

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,24 +4,30 @@ import axios from "axios";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null); //null = 未ログイン
+  const [user, setUser] = useState(null); // ユーザー情報
+  const [isLoading, setIsLoading] = useState(true); // ローディング状態
 
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const res = await axios.get("http://localhost:4000/api/v1/auth/me", {
-          withCredentials: true,
-        });
+        const res = await axios.get(
+          `${import.meta.env.VITE_TEST_URL}/api/v1/auth/me`,
+          {
+            withCredentials: true,
+          }
+        );
         setUser(res.data.user);
       } catch (err) {
         setUser(null);
+      } finally {
+        setIsLoading(false);
       }
     };
     checkAuth();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
